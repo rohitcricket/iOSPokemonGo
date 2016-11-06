cecab0485585354303db9da4b8a4fff6578cf8a9
ROHIT GUPTA <rohitgupta@ROHITs-MacBook-Pro-2.local>
2016-11-06 08:40:36 -0800

    created caught and uncaught sections in tableview.

--- PokemonGoClone/PokedexViewController.swift
+++ PokemonGoClone/PokedexViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class PokedexViewController: UIViewController {
+class PokedexViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     
     @IBOutlet weak var tableView: UITableView!
@@ -19,7 +19,8 @@ class PokedexViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        tableView.delegate = self
+        tableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
@@ -27,6 +28,41 @@ class PokedexViewController: UIViewController {
     @IBAction func mapTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "CAUGHT"
+        } else {
+            return "UNCAUGHT"
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return caughtPokemons.count
+        } else {
+            return uncaughtPokemons.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let pokemon : Pokemon
+        if indexPath.section == 0 {
+             pokemon = caughtPokemons[indexPath.row]
+        } else {
+             pokemon = uncaughtPokemons[indexPath.row]
+        }
+        
+        let cell = UITableViewCell()
+        cell.textLabel?.text = pokemon.name
+        cell.imageView?.image = UIImage(named: pokemon.imageName!)
+        return cell
+    }
 
 }
